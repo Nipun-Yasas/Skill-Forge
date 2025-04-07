7a4f5c01827ee8d7991eb344b6ec838049a320d8
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-04-07 11:08:23 +0530

    configured connection with db and created functional apis to login,signin and authenticate

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth',authRoutes);
+app.use('/api/dashboard',dashboardRoutes)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,(req,res)=>{
+    console.log(`Server is running on port ${PORT}`);
+})
